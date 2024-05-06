eeb8d6c1fe5900935b0867da4a9a3481538d3736
Rohit Kumar <168719487+RohitKumar-788@users.noreply.github.com>
2024-05-06 08:42:11 +0530

    adding first task "Number Game"

---
+++ NumberGame.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int attemptsLimit = 5;
+        int score = 0;
+
+        System.out.println("Welcome to Guess the Number Game!");
+
+        boolean playAgain = true;
+        while (playAgain) {
+            int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            System.out.println("I have selected a number between " + minRange + " and " + maxRange + ". Guess it!");
+
+            int attempts = 0;
+            while (attempts < attemptsLimit) {
+                System.out.print("Enter your guess number: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == randomNumber) {
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                    score++;
+                    break;
+                } else if (userGuess < randomNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (attempts == attemptsLimit) {
+                System.out.println("Sorry, you've used all your attempts. The correct number was " + randomNumber + ".");
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playChoice = scanner.next().toLowerCase();
+            if (!playChoice.equals("yes")) {
+                playAgain = false;
+            }
+        }
+
+        System.out.println("Game Over! Your total score: " + score);
+        scanner.close();
+    }
+}
